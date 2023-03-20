a9469a2dd76aa28991e6324ced79494a423f1bed
baysideDave <bhca.dave@gmail.com>
2023-03-20 12:25:29 -0400

    3/20 12:24 fixing reg page

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -105,10 +105,10 @@ def registration_request(request):
     # If it is a POST request
     elif request.method == 'POST':
         # Get user information from request.POST
-        username = request.POST['r_username']
-        password = request.POST['r_psw']
-        first_name = request.POST['fname']
-        last_name = request.POST['lname']
+        username = request.POST['username']
+        password = request.POST['psw']
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
         user_exist = False
         try:
             # Check if user already exists
