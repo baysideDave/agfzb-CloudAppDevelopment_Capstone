0b0116ab329b90b7587ddab415de49c97d559d4d
baysideDave <bhca.dave@gmail.com>
2023-03-25 14:54:28 -0400

    3/25 14:53 improving  views.py comments

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -10,7 +10,7 @@ from datetime import datetime
 import logging
 import json
 
-# Get an instance of a logger
+# Getting a logger instance
 logger = logging.getLogger(__name__)
 
 
@@ -35,8 +35,12 @@ def contact(request):
     if request.method == "GET":
         return render(request, 'djangoapp/contact.html', context)
 
+
+
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
+# IMPORTANT - this view uses the index.html template - it does not have
+# it's own template. It will indicate a bad login attempt on the index page, and it will
+# show the user as logged in when sucessful
 
 def login_request(request):
     context = {}
@@ -58,32 +62,12 @@ def login_request(request):
     else:
         return render(request, 'djangoapp/index.html', context)
     
-#def login_request(request):
-#    context = {}
-    #url = "https://??????.us-south.apigw.appdomain.cloud/api/dealership"
-    #dealerships = get_dealers_from_cf(url)
-    # Concat all dealer's short name
-    #context["dealership_list"]=dealerships
-#    if request.method == "POST":
-        # Get username and password from request.POST
-#        username = request.POST['username']
- #       password = request.POST['psw']
-        # Try to check if provide credential can be authenticated
- #       user = authenticate(username=username, password=password)
- #       if user is not None:
-            # If user is valid, call login method to login current user
- #           login(request, user)
- #           return render(request, 'djangoapp/index.html', context)
- #       else:
-            # If not, return to login page again
- #           context["message"]="Username or password is incorrect."
- #           return render(request, 'djangoapp/index.html', context)
- #   else:
- #       return render(request, 'djangoapp/index.html', context)
 
-# Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+
+# `logout_request` this view is used to handle sign a out request
+# IMPORTANT - this view uses the index.html template by redirecting to /djangoapp - it does not have
+# it's own template. It will indicate that no user is logged in on the index.html template
+# 
 def logout_request(request):
     # Get the user object based on session id in request
     print("Log out the user `{}`".format(request.user.username))
@@ -94,9 +78,12 @@ def logout_request(request):
 
     
 
-# Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
+# `registration_request` view  - handles a sign up request
+# from the nav-bar
+# NOTE - this vuew has it's own template but whwn unsucessful sends a message to the
+#        index page, and when sucessful, redireccts to the idex page which will show
+#        the newly registered user logged in
+#
 def registration_request(request):
     context = {}
     # If it is a GET request, just render the registration page
@@ -111,23 +98,26 @@ def registration_request(request):
         last_name = request.POST['reg-lastname']
         user_exist = False
         try:
-            # Check if user already exists
+            # Check to see if the user already exists
             User.objects.get(username=username)
             user_exist = True
         except:
-            # If not, simply log this is a new user
+            # If not an an existing user, we will log the username as a new user for debug purposes
             logger.debug("{} is new user".format(username))
         # If it is a new user
         if not user_exist:
-            # Create user in auth_user table
+            # Add the new user to th auth_user table
             user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
                                             password=password)
-            # Login the user and redirect to course list page
+            # Login the user and redirect to the index page
             login(request, user)
             return redirect("djangoapp:index")
         else:
             context["message"]="Account could not be created try again."
             return render(request, 'djangoapp/registration.html', context)
+
+
+
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
     def get_dealerships(request):
