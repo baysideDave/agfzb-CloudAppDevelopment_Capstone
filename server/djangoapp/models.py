0674bc2205324a4da63350d65cd7c42401de97f5
baysideDave <bhca.dave@gmail.com>
2023-05-11 10:45:38 -0400

    5-11-2023 added DealerReview class to models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -73,29 +73,20 @@ class CarDealer:
         
 # <HINT> Create a plain Python class `DealerReview` to hold review data
 
-#This is a stub to be finished
+class DealerReview:
 
-class     DealerReview:
-
-    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
-        # Dealer address
-        self.address = address
-        # Dealer city
-        self.city = city
-        # Dealer Full Name
-        self.full_name = full_name
-        # Dealer id
-        self.id = id
-        # Location lat
-        self.lat = lat
-        # Location long
-        self.long = long
-        # Dealer short name
-        self.short_name = short_name
-        # Dealer state
-        self.st = st
-        # Dealer zip
-        self.zip = zip
+    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year,sentiment, id):
+        self.dealership=dealership
+        self.name=name
+        self.purchase=purchase
+        self.review=review
+        self.purchase_date=purchase_date
+        self.car_make=car_make
+        self.car_model=car_model
+        self.car_year=car_year
+        self.sentiment=sentiment #from Watson NLU - will be positive, neutral, or negative.
+        self.id=id
 
     def __str__(self):
-        return "Dealer name: " + self.full_name
+        return "Review: " + self.review +\
+                " Sentiment: " + self.sentiment
