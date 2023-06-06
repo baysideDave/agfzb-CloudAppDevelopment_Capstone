e5163d118f3a48b8f30b15223beb9da7d85129ba
baysideDave <bhca.dave@gmail.com>
2023-06-06 02:22:52 -0400

    6-6-2023 2:21 last change before stopping

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -39,7 +39,7 @@ urlpatterns = [
     #path(route='add_review/<int:dealer_id>/', view=views.add_review, name="add_review"),
 
     #re_path(r'^add_review/',   view=views.add_review, name="dgxyz"),
-    path(route='dealer/<int:dealer_id>/add-review/', view=views.add_review, name="add_review"),
+    path(route='dealer/<int:dealer_id>/add_review/', view=views.add_review, name="add_review"),
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
